d594206371442f75ae9df6c15c154a3d3cdbcea8
Payam Azad <pvza85@gmail.com>
2016-07-21 11:43:47 +0300

    tensorflow

---
+++ src/feature_extractor/TensorFlowFeatureExtractor.py
@@ -0,0 +1,53 @@
+from AbstractFeatureExtractor import *
+import numpy as np
+import sys
+
+class TensorFlowFeatureExtractor(AbstractFeatureExtractor):
+    """
+    Simple Feature Extractor using one layer of a pretrained Deep Learning network
+    """
+    version = '0'
+    def __init__(self, dataset_path = None, folder_list = None, model_name = 'bvlc_reference_caffenet',
+                 layer_name = ['fc8'], caffe_root = '/home/ubuntu/workspace/caffe/', mode='cpu'):
+        AbstractFeatureExtractor.__init__(self, dataset_path, folder_list)
+        self.model_name = model_name
+        self.layer_name = layer_name
+        caffe_root = caffe_root
+        sys.path.insert(0, caffe_root + 'python')
+        global caffe
+        import caffe
+
+        model_prototxt = "{0}models/{1}/deploy.prototxt".format(caffe_root, model_name)
+        model_trained = "{0}models/{1}/{1}.caffemodel".format(caffe_root, model_name)
+        self.postfix = "_" + self.model_name.replace('/','') + '_' + self.layer_name.replace('/','') + '_' + self.version
+        if mode == 'gpu':
+            caffe.set_device(0)
+            caffe.caffe.set_mode_gpu()
+        else:
+            caffe.set_mode_cpu()
+
+        self.net = caffe.Classifier(model_prototxt, model_trained,
+                       channel_swap=(2,1,0),
+                       raw_scale=255,
+                       image_dims=(256, 256))
+
+    def save_features(self):
+        for f, sub in self.folder_list.iteritems():
+            self.folder_feature_extractor(f + 'Reference/')
+            for ff in sub:
+                self.folder_feature_extractor(ff)
+        return self.postfix
+
+    def folder_feature_extractor(self, folder_name):
+        output_file = "{0}features{1}.txt".format(folder_name, self.postfix)
+        with open(output_file, 'w') as writer:
+            writer.truncate()
+            for file_name in os.listdir(folder_name):
+                if file_name.endswith('.jpg'):
+                    file_name = folder_name + file_name
+                    if os.path.isfile(file_name):
+                        image = caffe.io.load_image(file_name)
+                        prediction = self.net.predict([image], oversample=False)
+                        res = []
+                        res = np.append(res, self.net.blobs[self.layer_name].data[0])
+                        np.savetxt(writer, res.reshape(1,-1), fmt='%.8g')
